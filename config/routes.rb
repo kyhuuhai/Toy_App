5cbbe62f1560325434746d42fe8bfba574feac5b
krylot <kyhuuhai@gmai.com>
2016-11-23 12:33:43 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 	
-  get 'users/new'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
-
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :microposts
   resources :users
   root 'users#index'
