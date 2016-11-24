d21cc84db6787b028bcde2f2aee65aabe2bb0b70
krylot <kyhuuhai@gmai.com>
2016-11-24 15:27:40 +0700

    chapter 9

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '5.0.0.1'
+
 gem 'puma',         '3.4.0'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.0.0'
@@ -10,9 +11,11 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.4.1'
 gem 'bootstrap-sass', '3.3.6'
 gem 'bcrypt',         '3.1.11'
+
 group :development, :test do
-  gem 'sqlite3', '1.3.11'
-  gem 'byebug',  '9.0.0', platform: :mri
+ gem 'pry-rails'
+ gem 'sqlite3', '1.3.11'
+ gem 'byebug',  '9.0.0', platform: :mri
 end
 gem 'rails-controller-testing'
 group :development do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,12 +63,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  #  test "email validation should reject invalid addresses" do
-  #   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-  #                          foo@bar_baz.com foo@bar+baz.com]
-  #   invalid_addresses.each do |invalid_address|
-  #     @user.email = invalid_address
-  #     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-  #   end
-  # end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
